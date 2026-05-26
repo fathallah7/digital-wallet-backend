a010339ebe88457284504d1bcead89b26f41638b
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-05-26 14:29:41 +0300

    feat: move JWT token generation to a separate service file

--- internal/handler/helpers.go
+++ internal/handler/helpers.go
@@ -4,10 +4,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"os"
-	"time"
-
-	"github.com/golang-jwt/jwt/v5"
 )
 
 type SuccessResponse struct {
@@ -45,14 +41,4 @@ func writeError(w http.ResponseWriter, status int, errors any, message string) {
 		Message: message,
 		Errors:  errors,
 	})
-}
-
-// jwt token
-func generateToken(userID string) (string, error) {
-	claims := jwt.MapClaims{
-		"user_id": userID,
-		"exp":     time.Now().Add(24 * time.Hour).Unix(),
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 }
