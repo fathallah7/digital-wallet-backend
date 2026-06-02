717fa152df974e39f27af33645398e4507e96f5a
Abdullah Fathallah <abdullahxoff@gmail.com>
2026-06-02 21:30:05 +0300

    refactor: reorganize Config struct fields for improved readability

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,20 +3,20 @@ package config
 import "os"
 
 type Config struct {
-	Port  string
-	DBUrl string
+	Port      string
+	DBUrl     string
+	JWTSecret string
 }
 
 func Load() Config {
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = ":8080"
 	}
 
 	return Config{
-		Port: port,
-		DBUrl: os.Getenv("DB_DSN"),
+		Port:      port,
+		DBUrl:     os.Getenv("DB_DSN"),
+		JWTSecret: os.Getenv("JWT_SECRET"),
 	}
-
 }
